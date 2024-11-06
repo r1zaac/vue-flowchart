5c7def9eb8498ae4e8350a9d1fd5081668b92575
r1zaac <muhammad.raza110@gmail.com>
2024-11-06 23:19:56 +0300

    fix: messageNode fixed

--- src/components/nodes/MessageNode.vue
+++ src/components/nodes/MessageNode.vue
@@ -1,13 +1,26 @@
+<template>
+  <BaseNode icon="✉️" title="Message" :description="messageDescription" />
+</template>
+
 <script setup>
+import { computed } from "vue";
+import { defineProps } from "vue";
 import BaseNode from "./BaseNode.vue";
 
-defineProps(["data"]);
-</script>
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true,
+  },
+});
 
-<template>
-  <BaseNode
-    icon="✉️"
-    title="Message"
-    :description="'Message: ' + (data.payload[0]?.text || 'Image Attachment')"
-  />
-</template>
+const messageDescription = computed(() => {
+  const payload = props.data?.payload;
+  if (payload && payload[0]) {
+    return `Message: ${
+      payload[0].text || payload[0].attachment || "No content"
+    }`;
+  }
+  return "Message: No content";
+});
+</script>
