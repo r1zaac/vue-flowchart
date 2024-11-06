047247d9adb68a72fdea173594c50e413612d5b7
r1zaac <muhammad.raza110@gmail.com>
2024-11-07 00:37:01 +0300

    feat: id routes created

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import FlowChartPage from "@/views/FlowChartPage.vue";
 
+const routes = [
+  { path: "/", name: "FlowChart", component: FlowChartPage },
+  { path: "/node/:id", name: "NodeDetails", component: FlowChartPage },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "FlowChart",
-      component: FlowChartPage,
-    },
-  ],
+  routes,
 });
 
 export default router;
