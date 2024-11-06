e16c89893c39d351654394e47f0116b1417e7e0d
r1zaac <muhammad.raza110@gmail.com>
2024-11-06 20:00:10 +0300

    refactor: data revised

--- src/initial-elements.js
+++ src/initial-elements.js
@@ -3,8 +3,14 @@ import payloadData from "./data/payload.json";
 export const initialNodes = payloadData.map((item) => ({
   id: item.id.toString(),
   type: item.type,
-  data: { label: item.name || item.data?.type },
-  position: { x: Math.random() * 400, y: Math.random() * 400 }, // Adjust positioning as needed
+  data: {
+    label: item.name || item.data?.type,
+    description: item.data?.type || "",
+    timezone: item.data?.timezone || "",
+    payload: item.data?.payload || [],
+    comment: item.data?.comment || "",
+  },
+  position: { x: Math.random() * 400, y: Math.random() * 400 },
 }));
 
 export const initialEdges = payloadData
