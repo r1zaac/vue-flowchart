f205a54166e40f84d055a473f7f9989ee098dbdc
r1zaac <muhammad.raza110@gmail.com>
2024-11-07 00:48:27 +0300

    fix: minor fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import FlowChartPage from "@/views/FlowChartPage.vue";
 
-const routes = [
-  { path: "/", name: "FlowChart", component: FlowChartPage },
-  { path: "/node/:id", name: "NodeDetails", component: FlowChartPage },
-];
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "FlowChart",
+      component: FlowChartPage,
+    },
+  ],
 });
 
 export default router;
